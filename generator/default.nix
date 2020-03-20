b4cbec1b63266da9772d460004a6bdb3c597d10c
Yannik Sander <me@ysndr.de>
2020-03-20 18:42:18 +0100

    Update build dependencies

--- generator/default.nix
+++ generator/default.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, filepath, process, hakyll, hakyll-sass, hsass, stdenv, text, time, pandoc, hakyll-images}:
+{ mkDerivation, base, filepath, process, hakyll, hakyll-sass, hsass, stdenv, text, time, pandoc, hakyll-images, blaze-html, blaze-markup}:
 mkDerivation {
   pname = "Site";
-  version = "0.1.0.0";
+  version = "0.1.1.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base filepath process time hakyll hakyll-sass hsass text pandoc hakyll-images];
+  executableHaskellDepends = [ base filepath process time hakyll hakyll-sass hsass text pandoc hakyll-images blaze-html blaze-markup];
   license = stdenv.lib.licenses.mit;
 }
