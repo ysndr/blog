8af88295e434696739c1199a76ed1ff08bbb6315
Yannik Sander <me@ysndr.de>
2020-10-27 12:42:59 +0100

    Flakify, add flake.nix and export artifacts
    - Flakes support builtin version locking of all inputs
    - hakyll needs version jailbreak for pandoc

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Flake utils demo";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nur.url = "github:nix-community/NUR";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+
+  inputs.uikit-src = {
+    url = "https://github.com/uikit/uikit/archive/v3.5.8.tar.gz";
+    flake = false;
+  };
+
+
+  outputs = { self, nixpkgs, nur, flake-utils, uikit-src }:
+    let
+    in
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            pkgs' = import nixpkgs { inherit system; };
+            blog = pkgs'.callPackage ./blog.nix {
+              pkgs = pkgs'; inherit nur;
+              thirdparty = [
+                {
+                  name = "uikit";
+                  path = "${uikit-src}/src";
+                }
+              ];
+            };
+          in
+            {
+              packages = { inherit (blog) generator generator-with-thirdparty ci shell; };
+              defaultPackage = blog.generator-with-thirdparty;
+              apps.generate-website =
+                flake-utils.lib.mkApp { drv = blog.ci.generate-website; };
+              defaultApp = blog.generate-website;
+              devShell = blog.shell;
+            }
+      );
+
+}
--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-{}: import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/88d9f776091.tar.gz";
-  sha256 = "sha256:0z8a0g69fmbbzi77jhvhwafv73dn5fg3gsr0q828lss6j5qpx995";
-}) {}
