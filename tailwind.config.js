c39b68e95f3e472ed7789f354a9f388269c626e1
Yannik Sander <me@ysndr.de>
2021-06-11 16:42:38 +0200

    Configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,21 +14,38 @@ module.exports = {
           'Fira Sans',
           defaultTheme.fontFamily.sans,
         ],
-        // serif: [
-        //   'Roboto Slab',
-        //   defaultTheme.fontFamily.serif
-        // ],
+        serif: [
+          "Merriweather",
+          "Roboto Slab",
+          defaultTheme.fontFamily.serif
+        ],
         'standout': [
             'Fira Code', 'monospace'
         ]
       },
       fontSize: {
-        body: ["15px", "22.5px"]
+        '2xs': ".5rem",
+        'xs': '.75rem',
+        'sm': '.875rem',
+        body: ["15px", "22.5px"],
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.375rem',
+       '3xl': '1.625rem',
+       '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+       '7xl': '5rem',
+
+        'lead': '1.125rem'
       },
       colors: {
           muted: "#f5f5f5",
           emphasis: "#444",
-          dark: "#222",
+          dark: "#252525",
+          hr: "#e5e5e5",
+          code: "var(--bg-code)",
+          highlight: "#5d8c91",
 
         // primary: 'var(--color-primary)',
         // secondary: 'var(--color-secondary)',
@@ -39,9 +56,26 @@ module.exports = {
         muted: "#999",
         mutedDark: "#b5b5b5",
         emphasizedDark: "#fff",
-        black: "#444"
-      }
+        black: "#252525",
+        code: "#3d545b",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '3rem',
+          xl: '4rem',
+          '2xl': '5rem',
+        },
+      },
+      height: {
+        xl: '32rem',
+        '2xl': '40rem',
+        '3xl': '48rem',
+       },
     },
+
   },
   variants: {},
   plugins: [],
